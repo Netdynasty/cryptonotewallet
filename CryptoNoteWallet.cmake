a69251d4b3a72de9e692baa6c2cb861a43a908e5
Netdynasty <38440148+Netdynasty@users.noreply.github.com>
2018-04-16 13:51:34 -0700

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "psalmscoin")
+set(CN_CURRENCY_DISPLAY_NAME "psalmscoin")
+set(CN_CURRENCY_TICKER "psalmscoin")
